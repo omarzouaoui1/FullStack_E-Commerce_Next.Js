33a658f08d752e62b7310233851dbb0a5a484d5b
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-09-06 10:42:08 +0100

    getting data and i m working on the fetch

--- ecommerce-admin/pages/api/products.js
+++ ecommerce-admin/pages/api/products.js
@@ -1,8 +1,12 @@
 import { mongooseConnect } from "@/lib/mongoose";
-import { Product } from "@/models/product";
+import { Product } from "@/models/Product";
 export default async function handle(req, res){
     const{method} = req;
     await mongooseConnect();
+
+    if(method === 'GET'){
+        res.json(await Product.find());
+    }
 
     if(method === 'POST'){
         const {title, description, price} = req.body;
--- ecommerce-admin/pages/products.js
+++ ecommerce-admin/pages/products.js
@@ -1,10 +1,40 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Products(){
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('/api/products').then(
+            response => {
+                setProducts(response.data);
+            }
+        )
+    }, []);
+
     return(
         <Layout>
             <Link className="bg-gray-400 text-white rounded-md py-1 px-2" href={'/products/new'}>Add New Product</Link>
+
+            <table className="basic">
+                <thead>
+                    <tr>
+                        <td>Product Name</td>
+                        <td></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr>
+                            <td>{product.title}</td>
+                            <td>button</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
     )
 }
