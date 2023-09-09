42e666ce409f0a5f4cd82c05a3dac824d99e9e98
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-09-09 12:23:26 +0100

    Delete Product

--- ecommerce-admin/pages/products/delete/[...id].js
+++ ecommerce-admin/pages/products/delete/[...id].js
@@ -23,12 +23,17 @@ export default function DeleteProductPage(){
         router.push('/products');
     }
 
+    async function deleteProduct(){
+        await axios.delete('/api/products?id='+id);
+        goBack();
+    }
+
     return(
         <Layout>
             <h1 className="text-center">Do you really want to delete &nbsp;'{productInfo?.title}'?</h1>
 
             <div className="flex gap-2 justify-center">
-            <button className="btn-red">Yes</button>
+            <button className="btn-red" onClick={deleteProduct}>Yes</button>
             <button className="btn-default" onClick={goBack}>No</button>
             </div>
             
