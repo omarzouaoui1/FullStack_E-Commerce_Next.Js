8d96f7225f723c6d0aea71500ee5d278a918ef92
omarzouaoui1 <omarzouaoui100@gmail.com>
2023-09-10 12:15:48 +0100

    Image Upload UI

--- ecommerce-admin/components/ProductForm.js
+++ ecommerce-admin/components/ProductForm.js
@@ -6,7 +6,8 @@ export default function ProductForm({
     _id,
     title : existingTitle, 
     description : existingDescription, 
-    price : existingPrice
+    price : existingPrice,
+    images,
 }){
     const router = useRouter();
 
@@ -45,6 +46,20 @@ export default function ProductForm({
             />
 
             <label>Photos</label>
+            <div className="mb-2">
+                <label className="w-60 h-60 text-center flex items-center justify-center text-sm gap-1 text-gray-700 rounded-md bg-gray-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                    </svg>
+                    <div>
+                    Upload 
+                    </div>
+                    <input type="file" className="hidden"/>
+                </label>
+                {!images?.length && (
+                <div>No Photos in this product</div>
+            )}
+            </div>
 
             <label>Description</label>
             <textarea 
